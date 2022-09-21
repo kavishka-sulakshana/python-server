6952fe53f8085d3a6686a4b330a780e81e25926b
Kavishka Sulakshana <kss20001218@gmail.com>
2022-09-21 17:54:55 +0530

    dev update

--- server.py
+++ server.py
@@ -42,32 +42,19 @@ while True:
     # Here I splitted the server request into parts from spaces and put that array into 'convReq' variable
     # the convReq[0] is the Request method (GET | POST)
     # the convReq[1] is the url routing path ex:-(/, /users, /login)
-
+    filePath = convReq[1]
     file = ''  # variable for read pages
     if (convReq[1] == '/'):   # this is the default route '/'
-        # Get the 'index.html' file from htdocs folder
-        file = open("htdocs/index.html", "r")
-        # sends the success response(status - 200) because server can find this webpage
+        filePath = '/index.html'
+    try :
+        file = open('htdocs'+filePath,'r')
         conn.send(bytes('HTTP/1.x 200 OK', 'utf8'))
-
-    elif (convReq[1] == '/profile'):  # this is a route '/users'
-        file = open("htdocs/users.html", "r")
-        conn.send(bytes('HTTP/1.x 200 OK', 'utf8'))
-    elif (convReq[1] == '/about'):
-        file = open("htdocs/aboutMe/about.html", "r")
-        conn.send(bytes('HTTP/1.x 200 OK', 'utf8'))
-    else:
-        file = open("htdocs/status/404.html", "r")  # this is page not found page
-        # page not found because a wrong url route.
+        conn.send(bytes('Content-Type : text/html\r\n', 'utf8'))
+        # The response content type is text/html
+        conn.send((bytes('\r\n', 'utf8')))
+        conn.send(bytes(file.read(), 'utf8'))
+        # Read the content of file variable and encode it to "utf8" and send to client
+    except :
         conn.send(bytes('HTTP/1.x 404 Not Found', 'utf8'))
-        # sends a (404 status) response
-
-    # The response content type is text/html
-    conn.send(bytes('Content-Type : text/html\r\n', 'utf8'))
-    conn.send((bytes('\r\n', 'utf8')))
-
-    conn.send(bytes(file.read(), 'utf8'))
-    # Read the content of file variable and encode it to "utf8" and send to client
-
     conn.close()
     # Close the connection with the client
